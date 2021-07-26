36d74b2577d14e38ae51e7a8e319187cd3862649
Oscar Eriksson <oscar@thenetcircle.com>
2021-07-26 16:12:12 +0800

    format MM event

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # sensu-mattermost-handler
+send alerts to mattermost
+
+## Configuration
+create a webhook in mattermost
+
+## Requirements
+
+## Usage
+```
+mattermost.py -u <url of webhook>
+```
---
+++ bin/mattermost.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python
+
+import argparse
+import sys
+import json
+import requests
+
+
+def main():
+    parser = argparse.ArgumentParser(description='push events to mattermost webhook')
+    parser.add_argument('-u', type=str, dest='url', required=True, help='the url to mattermost webhook')
+    args = parser.parse_args()
+
+    lines = sys.stdin.readlines()
+
+    data = ""
+    for line in lines:
+        data = data.join(line)
+
+    obj = json.loads(data)
+
+    host = obj['entity']['metadata']['name']
+    check_name = obj['check']['metadata']['name']
+    message = obj['check']['output']
+    occurrences = obj['check']['occurrences']
+
+    status = obj['entity']['status']
+    if status == 0:
+        status = ':white_check_mark: OK'
+    elif status == 1:
+        status = ':warning: WARNING'
+    elif status == 2:
+        status = ':stop_sign: CRITICAL'
+    else:
+        status = ':grey_question: UNKNOWN'
+
+    payload = {
+        "username": "SensuGo-Bot",
+        "icon_url": "https://raw.githubusercontent.com/sensu/web/828c7a0c2a6abb7ea215ca6ded903ba26045f542/logo.png",
+        "text": f"| **Server** | **{host}** |\n" +
+                f"| Check | {check_name} |\n" +
+                f"| Output | {message} |\n" +
+                f"| Occurrences | {occurrences} |\n" +
+                f"| Status | {status} |\n"
+    }
+
+    headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
+    try:
+        response = requests.post(
+            url=args.url,
+            data=json.dumps(payload),
+            headers=headers
+        )
+        print(response.text)
+    except Exception as e:
+        print(str(e))
+
+
+if __name__ == '__main__':
+    main()
